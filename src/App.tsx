40cc3c0bbd831142759034a51cc8963377511664
sammy-code98 <sammysmart98@yahoo.com>
2023-02-04 13:06:00 +0100

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -35,24 +35,3 @@ export default App
 
 
 
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAm2Mk8xfbK-WooUlWoO3quYkgaUPBFGGI",
-//   authDomain: "t-chatty.firebaseapp.com",
-//   projectId: "t-chatty",
-//   storageBucket: "t-chatty.appspot.com",
-//   messagingSenderId: "626296960845",
-//   appId: "1:626296960845:web:1d2a05b0cfd9174aff6904",
-//   measurementId: "G-WSYPSL5MLC"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
