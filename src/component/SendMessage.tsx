74eaeb36ac49295cbff60f24d1f5de7cf1872fac
sammy-code98 <sammysmart98@yahoo.com>
2023-02-16 11:57:11 +0100

    .

--- src/component/SendMessage.tsx
+++ src/component/SendMessage.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { auth, db } from "../services/firebase"
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+
 export default function SendMessage({ scroll }: any) {
+    const [message, setMessage] = useState("")
+
+    const sendMessage = async (event: any) => {
+        event.preventDefault()
+        if (message.trim() === "") {
+            alert("Enter Valid message")
+            return;
+        }
+        const { uid, displayName, photoUrl } = auth.currentUser
+        await addDoc(collection(db, "messages"), {
+            text: message,
+            name: displayName,
+            avatar: photoUrl,
+            createdAt: serverTimestamp(),
+            uid,
+        })
+        setMessage("")
+        scroll.current.scrollIntoView({ behaviour: 'smooth' })
+    }
     return (
         <Box >
             <Box
                 as='form'
+                onSubmit={sendMessage}
                 display='flex'
                 justifyContent='space-between'
                 px='2rem'
@@ -13,9 +36,10 @@ export default function SendMessage({ scroll }: any) {
                 bottom='0px'
                 w='100%'
                 alignItems='center' gap='2'>
-            <FormControl>
-                    <FormLabel hidden> Enter Message</FormLabel>
-                    <Input variant='outline' type='text' placeholder='type message...' />
+                <FormControl>
+                    <Input variant='outline' type='text' placeholder='type message...' value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
                 </FormControl>
 
                 <Button type="submit" variant='outline' bgColor='blue.300'>Send</Button>
